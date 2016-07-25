c8b3ca4aa6c7c6e2fd940ad31bdc3f3395d62f23
soliedra <mrjaviercasado@yahoo.es>
2016-07-25 10:25:15 +0200

    I had forgoten the example

--- examples/MoveServoToPosition/MoveServoToPosition.ino
+++ examples/MoveServoToPosition/MoveServoToPosition.ino
@@ -1,40 +1,45 @@
 /*
  * Turns the Servo-motor shaft to the position set by the user through the serial port.
  *
- * Hardware:
+ * This library uses PID and Encoder feedback to control a DC motor. It's modeled a little bit after the AccelStepper library.
+ *
+ * Pololu VNH5019 Driver has been used. 
+ *
+ *
+ * DC motor driver connections
+ *-----------------------------
+ * 
+ * Function         | Pin   
+ *----------------------------
+ * Direcion A (CW)  | D5	 
+ *----------------------------
+ * Direcion B (CCW) | D7 	  
+ *----------------------------
+ * PWM	            | D6	  
+ *------------------------------------------
+ * Brake (GND)      | D5 and D7 set to LOW  
+ *---------------------------------------------
+ * Current sensing  | A1      
+ ------------------------------
+ *
+ * 
+ * Hardware
  * --------
- * Modified [Arduino Motor Shield](https://www.arduino.cc/en/Main/ArduinoMotorShieldR3). Male pins have been cut off
- * and pin D3 and D5 have been bridged on the top side of the shield. Pins D2 and D3 have been left free in Arduino
- * to connect the encoder feedback. The outputs of channels A and B have been connected in parallel to provide 4 Amps.
- *
+ * [Pololu VNH5019 Driver](https://www.pololu.com/product/1451) 
+ * Arduino UNO R3
  * A 12V geared DC motor from a battery drill. Output speed aprox 600rpm
- *
  * A DIY Quad encoder made with with two [Pololu QTR-1RC Reflectance sensors](https://www.pololu.com/product/2459) that
  * provides 8CPR.
  *
- * Connections:
- * ------------
- * Arduino motor shield connections:
- * 
- * Function         | Channel A   | Channel B |
- *---------------------------------------------
- * Direcion         | D12 	      | D13       |
- *---------------------------------------------
- * PWM	            | D5 (brigded | D11       |
- *                  | to D3)      |           |
- ----------------------------------------------
- *
- * When the Direction pins are HIGH the motor turns CCW and
- * CW when LOW.
- * 
  * Front view of the encoders location
  *
- *       B
- *      ***
- *     *   * A
- *      ***
- *  CW <----> CCW
- *
+ *        B
+ *       ***
+ *      *   * A
+ *       ***
+ * CW(+)<---> CCW (-)
+ * 
+ *  Connections:
  *  Encoder A -> D2 (Arduino, not motor shield)
  *  Encoder B -> D3 (Arduino, not motor shield)
  *
@@ -43,28 +48,35 @@
  *  Encoder A  ___|      |______|      |______
  *                    ______        ______
  *  Encoder B  ______|      |______|      |______
- *             CCW -->(-)                  (+) <--CW
+ *             CCW-->(-)                   (+) <--CW
  *
- * Author: Javier Casado July 2016
- * License: CC SHA BY
+ * 
+ * Pins
+ * ----
+ * The Driver pins have been connected as described above. 
+ * The Quad encoder channel B (North side of the motor shaft, front view) connected to pin D3 (external interrupt)
+ * The Quad encoder channel B (East side of the motor shaft, front view) connected to pin D2 (external interrupt) 
+ * 
+ * Modified by Javier Casado July 2016
  */
 #include <Encoder.h>
 #include <PID_v1.h>
 #include <DCMotorServo.h>
 #include <MotorDriver.h>
-#include <ArduinoMotorShieldDriver.h>
+#include <VNH5019MotorDriver.h>
 
-// Connections
-#define pin_dcmoto_dirA 12
-#define pin_dcmoto_dirB 13
-#define pin_dcmoto_pwm_outA 5
-#define pin_dcmoto_pwm_outB 11
+
+#define pin_dcmoto_dirA 5
+#define pin_dcmoto_dirB 7
+#define pin_dcmoto_pwm  6
 #define pin_dcmoto_encodeA 2
 #define pin_dcmoto_encodeB 3
 
-Encoder encoder = Encoder(pin_dcmoto_encodeB,pin_dcmoto_encodeA);
-ArduinoMotorShieldDriver  driver  = ArduinoMotorShieldDriver(pin_dcmoto_dirA, pin_dcmoto_dirB, pin_dcmoto_pwm_outA,pin_dcmoto_pwm_outB);
-DCMotorServo servo = DCMotorServo(&driver, &encoder);
+VNH5019MotorDriver driver = VNH5019MotorDriver(pin_dcmoto_dirA,pin_dcmoto_dirB,pin_dcmoto_pwm);
+
+Encoder myEncoder = Encoder(pin_dcmoto_encodeB,pin_dcmoto_encodeA);
+
+DCMotorServo servo = DCMotorServo(&driver, &myEncoder);
 
 int targetPosition = 0;
 
@@ -73,10 +85,10 @@ void setup() {
   //Tune the servo feedback
   //Determined by trial and error
   //servo.myPID->SetTunings(0.1,0.15,0.05);
-  servo.myPID->SetTunings(0.45,0,0.1);
+  servo.myPID->SetTunings(0.45,0.0,0.0);
   servo.myPID->SetSampleTime(50);  
-  servo.setPWMSkip(80);
-  servo.setAccuracy(8);
+  servo.setPWMSkip(50);
+  servo.setAccuracy(1);
   //Un-necessary, initializes to 0:
   //servo.setCurrentPosition(0);
   
@@ -96,6 +108,7 @@ void loop() {
 /*
  * Returns true when a new position has been read from
  * the Serial port and false otherwise.
+ * The value read is stored in the global variable targetPosition.
  */
 bool readTargetPosition()
 {
@@ -104,7 +117,7 @@ bool readTargetPosition()
 		//wait for the incomming data to arrive, avoids partial readings
 		delay(10);
 		targetPosition = Serial.parseInt();
-		//There's an end of string read as a zero, remove it reading again 
+		//Maybe there's an end of string read as a zero, remove it reading again 
 		Serial.parseInt();
 		Serial.print("Position: ");
 		Serial.println(targetPosition);
